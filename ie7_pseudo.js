8454bc4e4f89406c7e8c4aff0eecb3c69046f0a0
Kevin Dees <kevin@robojuice.com>
2011-09-13 09:07:44 -0400

    added content support

--- ie7_pseudo.js
+++ ie7_pseudo.js
@@ -1,10 +1,12 @@
 (function() {
-	function set_after(rule, style) {
-		$(rule).parent().append($(document.createElement('ieae')).css(style));
+	function set_after(rule, style, content) {
+		var ieae = $(document.createElement('ieae')).css(style).append( document.createTextNode(content) );
+		$(rule).parent().append(ieae);
 	}
 
-	function set_before(rule, style) {
-		$(rule).parent().prepend($(document.createElement('iebe')).css(style));
+	function set_before(rule, style, content) {
+		var iebe = $(document.createElement('iebe')).css(style).append( document.createTextNode(content) );
+		$(rule).parent().prepend(iebe);
 	}
 	
 	var css = document.styleSheets;
@@ -16,14 +18,18 @@
 				var str = rules[--before].selectorText;
 				// next line needs love
 				str = /(.+):/.exec(str)[1];
-				set_before(str, rules[x].style);
+				var content = rules[x].style.content;
+				content = content.substring(0, content.length-1).substring(1);
+				set_before(str, rules[x].style, content);
 			}
 			else if(rules[x].selectorText.indexOf("iea") == 0) {
 				var before = x;
 				var str = rules[--before].selectorText;
 				// next line needs love
 				str = /(.+):/.exec(str)[1];
-				set_after(str, rules[x].style);
+				var content = rules[x].style.content;
+				content = content.substring(0, content.length-1).substring(1);
+				set_after(str, rules[x].style, content);
 			}
 		}
 	}
