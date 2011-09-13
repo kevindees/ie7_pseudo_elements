3e7758623e9788185fbfe75590c4e889ca13a28a
Kevin Dees <kevin@robojuice.com>
2011-09-13 18:53:10 -0400

    element stacking, to specificity yet

--- ie7_pseudo.js
+++ ie7_pseudo.js
@@ -1,12 +1,26 @@
 (function() {
 	function set_after(rule, style, content) {
-		var ieae = $(document.createElement('ieae')).css(style).append( document.createTextNode(content) );
-		$(rule).parent().append(ieae);
+		if($(rule).hasClass('iea')) {
+			var parent = $(rule).parent();
+			$(parent).find('ieae').css(style);
+		}
+		else {
+			$(rule).addClass('iea');
+			var ieae = $(document.createElement('ieae')).css(style).append( document.createTextNode(content) );
+			$(rule).after(ieae);	
+		}
 	}
 
 	function set_before(rule, style, content) {
-		var iebe = $(document.createElement('iebe')).css(style).append( document.createTextNode(content) );
-		$(rule).parent().prepend(iebe);
+		if($(rule).hasClass('ieb')) {
+			var parent = $(rule).parent();
+			$(parent).find('iebe').css(style);
+		}
+		else {
+			$(rule).addClass('ieb');
+			var iebe = $(document.createElement('iebe')).css(style).append( document.createTextNode(content) );
+			$(rule).before(iebe);		
+		}
 	}
 	
 	var css = document.styleSheets;
@@ -15,21 +29,21 @@
 		for(var x = 0, y = rules.length; x < y; x++) {
 			if(rules[x].selectorText.indexOf("ieb") == 0) {
 				var before = x;
-				var str = rules[--before].selectorText;
+				var ruleStr = rules[--before].selectorText;
 				// next line needs love
-				str = /(.+):/.exec(str)[1];
+				ruleStr = /(.+):/.exec(ruleStr)[1];
 				var content = rules[x].style.content;
 				content = content.substring(0, content.length-1).substring(1);
-				set_before(str, rules[x].style, content);
+				set_before(ruleStr, rules[x].style, content);
 			}
 			else if(rules[x].selectorText.indexOf("iea") == 0) {
 				var before = x;
-				var str = rules[--before].selectorText;
+				var ruleStr = rules[--before].selectorText;
 				// next line needs love
-				str = /(.+):/.exec(str)[1];
+				ruleStr = /(.+):/.exec(ruleStr)[1];
 				var content = rules[x].style.content;
 				content = content.substring(0, content.length-1).substring(1);
-				set_after(str, rules[x].style, content);
+				set_after(ruleStr, rules[x].style, content);
 			}
 		}
 	}
