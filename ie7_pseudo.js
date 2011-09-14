a75a32389519d1057fb211d83f44bbbfc01b64ec
Kevin Dees <kevin@robojuice.com>
2011-09-13 22:18:19 -0400

    eibe and ae slecet bug fixed, with next and prev

--- ie7_pseudo.js
+++ ie7_pseudo.js
@@ -1,37 +1,33 @@
 (function() {
-
-	function set_element(rule, style, content, newRelID, iee) {
+	function set_element(rule, style, content, iee) {
 		if($(rule).hasClass(iee)) {
-			var parent = $(rule).parent();
 			if(content) {
-				$(parent).find(iee+'e').css(style).html(content);
+				if(iee == 'ieb') {
+					$(rule).prev(iee+'e').css(style).html(content); }
+				else {
+					$(rule).next(iee+'e').css(style).html(content);
+				}
 			}
 			else {
-				$(parent).find(iee+'e').css(style);
+				if(iee == 'ieb') {
+					$(rule).prev(iee+'e').css(style); }
+				else {
+					$(rule).next(iee+'e').css(style);
+				}
 			}
 		}
 		else {
-			var bob = $(rule).length;
-			for(q =0; q < bob; q++) {
-				$($(rule)[q]).addClass(iee + ' ' + newRelID + q);
-			}
+			$(rule).addClass(iee);
 			var ieeNew = $(document.createElement(iee+'e')).css(style).append( document.createTextNode(content) );
 			if(iee == 'ieb') {
 				$(rule).before(ieeNew); }
 			else {
 				$(rule).after(ieeNew);
-			}		
+			}
 		}
-	}
-
-	function find_relIDs(ruleClasses) {
-		var ruleStr = / *((\.)ieid-[0-9])/.exec(ruleClasses);
-		alert(ruleStr)
-		return ruleStr;
 	}
 	
 	var css = document.styleSheets;
-	var relID = 0;
 	for(var i = 0, j = css.length; i < j; i++) {
 		var rules = css[i].rules;
 		for(var x = 0, y = rules.length; x < y; x++) {
@@ -42,8 +38,7 @@
 				ruleStr = /(.+):/.exec(ruleStr)[1];
 				var content = rules[x].style.content;
 				content = content.substring(0, content.length-1).substring(1);
-				var newRelID = 'ieid-' + relID++;
-				set_element(ruleStr, rules[x].style, content, newRelID, 'ieb');
+				set_element(ruleStr, rules[x].style, content, 'ieb');
 			}
 			else if(rules[x].selectorText.indexOf("iea") == 0) {
 				var before = x;
@@ -52,8 +47,7 @@
 				ruleStr = /(.+):/.exec(ruleStr)[1];
 				var content = rules[x].style.content;
 				content = content.substring(0, content.length-1).substring(1);
-				var newRelID = 'ieid-' + relID++;
-				set_element(ruleStr, rules[x].style, content, newRelID, 'iea');
+				set_element(ruleStr, rules[x].style, content, 'iea');
 			}
 		}
 	}
