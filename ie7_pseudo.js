f702d48a69d0682a33dfc5ea4c5bf251d1b8c957
Kevin Dees <kevin@robojuice.com>
2011-09-17 14:14:03 -0400

    better support for content (no images yet), made cascading better

--- ie7_pseudo.js
+++ ie7_pseudo.js
@@ -1,49 +1,56 @@
 (function() {
-		function set_element(rule, style, content, iee) {
-		if($(rule).hasClass(iee)) {
-			if(content) {
-				if(iee == 'ieb') {
-					$(rule + ' > ' + iee + 'e').css(style).html(content); }
-				else {
-					$(rule + ' > ' + iee + 'e').css(style).html(content); }
-			}
-			else {
-				if(iee == 'ieb') {
-					$(rule + ' > ' + iee + 'e').css(style); }
-				else {
-					$(rule + ' > ' + iee + 'e').css(style); }
-			}
-		}
-		else {
-			$(rule).addClass(iee);
-			var ieeNew = $(document.createElement(iee+'e')).css(style).append( document.createTextNode(content) );
-			if(iee == 'ieb') {
-				$(rule).prepend(ieeNew); }
-			else {
-				$(rule).append(ieeNew); }
-		}
-	}
-	
-	var css = document.styleSheets;
-	for(var i = 0, j = css.length; i < j; i++) {
-		var rules = css[i].rules;
-		for(var x = 0, y = rules.length; x < y; x++) {
-			if(rules[x].selectorText.indexOf("ieb") == 0) {
-				var before = x;
-				var ruleStr = rules[--before].selectorText;
-				ruleStr = /(.+):un/.exec(ruleStr)[1];
-				var content = rules[x].style.content;
-				content = content.substring(0, content.length-1).substring(1);
-				set_element(ruleStr, rules[x].style, content, 'ieb');
-			}
-			else if(rules[x].selectorText.indexOf("iea") == 0) {
-				var before = x;
-				var ruleStr = rules[--before].selectorText;
-				ruleStr = /(.+):un/.exec(ruleStr)[1];
-				var content = rules[x].style.content;
-				content = content.substring(0, content.length-1).substring(1);
-				set_element(ruleStr, rules[x].style, content, 'iea');
-			}
-		}
-	}
+    function set_element(rule, style, content, iee) {
+        if($(rule).hasClass(iee)) {
+            if(content) {
+                var oldStyle = $(rule + ' > ' + iee + 'e').attr('style');
+                $(rule + ' > ' + iee + 'e').css(style).html(content);
+                var newStyle = $(rule + ' > ' + iee + 'e').attr('style');
+                $(rule + ' > ' + iee + 'e').attr('style', oldStyle + ';' + newStyle )
+            }
+            else {
+                var oldStyle = $(rule + ' > ' + iee + 'e').attr('style');
+                $(rule + ' > ' + iee + 'e').css(style);
+                var newStyle = $(rule + ' > ' + iee + 'e').attr('style');
+                $(rule + ' > ' + iee + 'e').attr('style', oldStyle + ';' + newStyle )
+            }
+        }
+        else {
+            $(rule).addClass(iee);
+            var ieeNew = $(document.createElement(iee+'e')).css(style).append( document.createTextNode(content) );
+            if(iee == 'ieb') {
+                $(rule).prepend(ieeNew); }
+            else {
+                $(rule).append(ieeNew); }
+        }
+    }
+    
+    function trim_content(content) {
+        if(content == "\"\"" || content == "\'\'" || content == null) {
+        content = ''; } else {
+        content = content.substring(0, content.length-1).substring(1); }
+        return content;
+    }
+    
+    var css = document.styleSheets;
+    for(var i = 0, j = css.length; i < j; i++) {
+        var rules = css[i].rules;
+        for(var x = 0, y = rules.length; x < y; x++) {
+            if(rules[x].selectorText.indexOf("ieb") == 0) {
+                var before = x;
+                var ruleStr = rules[--before].selectorText;
+                ruleStr = /(.+):un/.exec(ruleStr)[1];
+                var content = rules[x].style.content;
+                content = trim_content(content);
+                set_element(ruleStr, rules[x].style, content, 'ieb');
+            }
+            else if(rules[x].selectorText.indexOf("iea") == 0) {
+                var before = x;
+                var ruleStr = rules[--before].selectorText;
+                ruleStr = /(.+):un/.exec(ruleStr)[1];
+                var content = rules[x].style.content;
+                content = trim_content(content);
+                set_element(ruleStr, rules[x].style, content, 'iea');
+            }
+        }
+    }
 })();
